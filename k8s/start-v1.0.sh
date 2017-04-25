a117049d1d765c098fd492724b0d5bbb5d0271f2
pmcgrath <pmcgrath@gmail.com>
2017-04-25 11:10:55 +0100

    Used kubectl create -f for the entire dircectory

--- k8s/start-v1.0.sh
+++ k8s/start-v1.0.sh
@@ -3,4 +3,4 @@
 namespace=$(grep 'name: ' 00-namespace.yaml | cut -f4 -d ' ')
 
 # Apply all yaml files
-for file in `ls *.yaml`; do kubectl create -f $file --namespace $namespace --record; done
+kubectl create -f $file . --namespace $namespace --record
